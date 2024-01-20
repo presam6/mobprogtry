858dd7510a2d5ad86c686ce3eef8ab6a87db4990
presam6 <maglangit.mjosh@gmail.com>
2024-01-20 12:02:44 +0800

    Add assets and finish button or businesscategoryscreen basta kato

--- App/Screens/ListByCategoryScreen/BusinessListItem.jsx
+++ App/Screens/ListByCategoryScreen/BusinessListItem.jsx
@@ -1,19 +1,48 @@
 import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
+import Colors from '../../Utils/Colors'
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function BusinessListItem({ business }) {
   return (
-    <View>
+    <View style={styles.container}>
       <Image source={{ uri: business?.image?.url }}
         style={styles.image}
       />
+      <View style={styles.subContainer}>
+        <Text style={{ fontFamily: 'outfit-bold', fontSize: 15}}> {business.name} </Text>
+        <Text style={{ fontFamily: 'outfit', color: Colors.GRAY, fontSize: 12}}>
+        <Ionicons name="person" size={12} color={Colors.PRIMARY} /> {business.contactPerson} </Text>
+        <Text style={{ fontFamily: 'outfit', color: Colors.GRAY, fontSize: 12}}>
+        <MaterialIcons name="alternate-email" size={12} color={Colors.PRIMARY} marginBottom={10} /> {business.email} </Text>
+        <Text style={{ fontFamily: 'outfit', fontSize: 12, color: Colors.GRAY }}>
+          <Ionicons name="location-sharp" size={15} color={Colors.PRIMARY} /> {business.address} </Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: Colors.WHITE,
+    borderRadius: 15,
+    marginBottom: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 10,
+    alignItems: 'center'
+  },
+  subContainer: {
+    display: 'flex',
+    gap: 7,
+  },
   image: {
     width: 100,
     height: 100,
+    borderColor: Colors.BLACK,
+    borderRadius: 15,
+    backgroundColor: Colors.LIGHT_PRIMARY,
   },
 })
--- App/Screens/ListByCategoryScreen/ListByCategoryScreen.jsx
+++ App/Screens/ListByCategoryScreen/ListByCategoryScreen.jsx
@@ -4,6 +4,7 @@ import { useNavigation, useRoute } from '@react-navigation/native'
 import { AntDesign } from '@expo/vector-icons';
 import GlobalAPI from '../../Utils/GlobalAPI';
 import BusinessListItem from './BusinessListItem';
+import Colors from '../../Utils/Colors';
 
 export default function ListByCategoryScreen() {
   const param = useRoute().params;
@@ -33,12 +34,15 @@ export default function ListByCategoryScreen() {
         <AntDesign name="back" size={30} color="black" />
         <Text style={{ fontSize: 25, fontFamily: 'outfit-medium' }}> {param?.category} </Text>
       </TouchableOpacity>
-      <FlatList
+      
+      {businessList?. length > 0? <FlatList
       data={businessList}
       renderItem={({item, index}) => (
         <BusinessListItem business={item}/>
       )}
-      />
+      />:
+      <Text style={{fontFamily: 'outfit-medium', fontSize: 20, textAlign: 'center', marginTop: '20%', 
+      color: Colors.GRAY}}> No Business Found </Text>}
     </View>
   )
 }
